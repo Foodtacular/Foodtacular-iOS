6be7191807fbd9cf63e8bfe3cc2c11f6f3a06193
Alfredo <icealfredo123@gmail.com>
2021-11-12 16:15:51 -0600

    commit

--- Foodtacular/AppDelegate.swift
+++ Foodtacular/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Parse
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,10 +15,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+ 
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "qEBVyzXCHaCxf38jCbmAvoYFjonkJ2jCvDdK6YLP" // <- UPDATE
+                $0.clientKey = "kKW18Q02SWObt6K02ryW4QjiacOEbPlPnDkDi5L5" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
+        
+        
         // Override point for customization after application launch.
         return true
     }
-
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
